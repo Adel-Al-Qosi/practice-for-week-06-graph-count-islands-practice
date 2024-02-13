c9134816d2b0a73f1840a1110176f9b1e0f9e416
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2024-02-13 03:55:17 +0300

    end practice

--- island.js
+++ island.js
@@ -1,57 +1,97 @@
 function getNeighbors(row, col, matrix) {
-  
-  // Check top
-  // Check top right
-  // Check right
-  // Check bottom right
-  // Check bottom
-  // Check bottom left
-  // Check left
-  // Check top left
-  // Return neighbors
-  
-  // Your code here
+  const neighbors = [];
+
+  if (row > 0 && matrix[row - 1][col] === 1) {
+    const top = [row - 1, col];
+    neighbors.push(top);
+  }
+
+  if (row > 0 && col < matrix[row].length - 1 && matrix[row - 1][col + 1] === 1) {
+    const topRight = [row - 1, col + 1];
+    neighbors.push(topRight);
+  }
+
+  if (col < matrix[row].length - 1 && matrix[row][col + 1] === 1) {
+    const right = [row, col + 1];
+    neighbors.push(right);
+  }
+
+  if (row < matrix.length - 1 && col < matrix[row].length - 1 && matrix[row + 1][col + 1] === 1) {
+    const bottomRight = [row + 1, col + 1];
+    neighbors.push(bottomRight);
+  }
+
+  if (row < matrix.length - 1 && matrix[row + 1][col] === 1) {
+    const bottom = [row + 1, col];
+    neighbors.push(bottom);
+  }
+
+  if (row < matrix.length - 1 && col > 0 && matrix[row + 1][col - 1] === 1) {
+    const bottomLeft = [row + 1, col - 1];
+    neighbors.push(bottomLeft);
+  }
+
+  if (col > 0 && matrix[row][col - 1] === 1) {
+    const left = [row, col - 1];
+    neighbors.push(left);
+  }
+
+  if (row > 0 && col > 0 && matrix[row - 1][col - 1] === 1) {
+    const topLeft = [row - 1, col - 1];
+    neighbors.push(topLeft);
+  }
+
+  return neighbors;
 }
 
 function countIslands(matrix) {
-  
-  // Create a visited set to store visited nodes
-  // Initialize count to 0
-  // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
-    // increment island count and start traversing neighbors
-      // DO THE THING (increment island count by 1)
-      // Initialize a stack with current index
-      // Add stringified version of current index to the visited set
-      // While stack contains elements
-        // Pop element from stack
-        // Get valid neighbors of current element
-        // Iterate over neigbors
-          // If neighbor has not been visited
-            // Add neighbor to stack
-            // Mark neighbor as visited
-  // Return island count
-  
-  // Your code here
+  const visited = new Set();
+  let count = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const current = [i, j];
+      const key = current.join(',');
+
+      if (!visited.has(key) && matrix[i][j] === 1) {
+        count++;
+        const queue = [current];
+
+        while (queue.length > 0) {
+          const [row, col] = queue.shift();
+          const neighbors = getNeighbors(row, col, matrix);
+
+          for (const neighbor of neighbors) {
+            const [nr, nc] = neighbor;
+            const neighborKey = neighbor.join(',');
+
+            if (!visited.has(neighborKey) && matrix[nr][nc] === 1) {
+              queue.push(neighbor);
+              visited.add(neighborKey);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return count;
 }
 
 // Uncomment the lines below for local testing
 // const matrix = [
-//                 [1,1,1,0,0],
-//                 [0,1,1,0,1],
-//                 [0,1,1,0,1]
-//               ]
-
-// console.log(getNeighbors(1, 1, matrix)); // [[0, 0], [0, 1], [0, 2], [1, 2], [2, 1], [2, 2]]
-// console.log(getNeighbors(2,4, matrix)) // [[1,4]]
+//   [1, 1, 1, 0, 0],
+//   [0, 1, 1, 0, 1],
+//   [0, 1, 1, 0, 1]
+// ];
 
 // const matrix2 = [
-//                     [1,1,1,0,1],
-//                     [0,0,0,0,1],
-//                     [1,0,0,1,0],
-//                 ]
+//   [1, 1, 1, 0, 1],
+//   [0, 0, 0, 0, 1],
+//   [1, 0, 0, 1, 0],
+// ];
 
-// console.log(countIslands(matrix)) // 2
+// console.log(countIslands(matrix)); // 2
 // console.log(countIslands(matrix2)); // 3
 
 module.exports = [countIslands, getNeighbors];
